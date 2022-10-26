32d1d60f8178ec964162275d6479c04723365858
cesarcuadro <cesarcuadro777@gmail.com>
2022-10-26 13:58:25 -0700

    last couple of challenges need to be completed

--- jsintro-cesar-blake.js
+++ jsintro-cesar-blake.js
@@ -48,12 +48,16 @@ console.log(title.toLowerCase()[0] === "l")
 
 // Consider the variables:
 
-// var theQuestion = "life, the universe, and everything"
-// var theAnswer = 42
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
 // Write the code that will log theAnswer divided by 2.
+console.log(theAnswer / 2);
 // Write the code that will log the length of theQuestion.
+console.log(theQuestion.length);
 // Write the code that will log the index of the character "f" in the theQuestion.
+console.log(theQuestion[2])
 // Write the code that will log the concatenation of the two variables.
+console.log(``)
 // Write the code that will log "the universe".
 // Write the code that will log the character "l" from theQuestion.
 // Write the code that will log whether theQuestion.length is greater than theAnswer
