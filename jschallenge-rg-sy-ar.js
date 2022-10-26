ab78a1d69d54b1133905777db83f28077a7dfd11
renitagid <renita.y.ip@gmail.com>
2022-10-26 16:34:50 -0700

    finished more questions

--- jschallenge-rg-sy-ar.js
+++ jschallenge-rg-sy-ar.js
@@ -43,17 +43,23 @@ console.log("LEARN"[0] === "l");
 // Consider the variables:
 console.log("LEARN"[0] == "L");
 
-// var theQuestion = "life, the universe, and everything"
-
-// var theAnswer = 42
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
 
 // Write the code that will log theAnswer divided by 2.
+console.log(theAnswer / 2);
 // Write the code that will log the length of theQuestion.
+console.log(theQuestion.length);
 // Write the code that will log the index of the character "f" in the theQuestion.
+console.log(theQuestion.indexOf("f"))
 // Write the code that will log the concatenation of the two variables.
+console.log(`${theAnswer}` + `${theQuestion}`)
 // Write the code that will log "the universe".
+console.log(theQuestion.slice(6, 18))
 // Write the code that will log the character "l" from theQuestion.
+console.log(theQuestion.charAt(0))
 // Write the code that will log whether theQuestion.length is greater than theAnswer.
+console.log(theQuestion.length > theAnswer)
 
 // Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
 let item = 50;
