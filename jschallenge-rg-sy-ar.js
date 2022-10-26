63049c65f11472a5125e5d95f3d24b9cae89948f
renitagid <renita.y.ip@gmail.com>
2022-10-26 13:58:05 -0700

    halfway through second section

--- jschallenge-rg-sy-ar.js
+++ jschallenge-rg-sy-ar.js
@@ -11,22 +11,28 @@ console.log(5 ** 7)
 // Write the code that will log the whole number remainder of 33 divided by 6.
 console.log(33 % 6)
 // Write the code that will log the length of a string containing your name.
-console.log(renita.length)
+console.log("renita".length)
 // Write the code that will log whether your string includes the letter "e"?
-console.log(renita.includes("e"))
+console.log("renita".includes("e"))
 // Write the code that will log the character at the first index of the string.
-console.log(renita.charAt(0))
+console.log("renita".charAt(0))
 // Write the code that will log the string in all uppercase letters.
-console.log(renita.toUpperCase())
+console.log("renita".toUpperCase())
 
 // Write the code that will log true or false for the following:
 
 // Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 >= 67 / 2)
 // Does 5 evaluate to the same as "5"?
+console.log(5 == "5")
 // Does 5 strictly equal "5"?
+console.log(5 === "5")
 // Does !3 strictly equal 3?
+console.log(!3 === 3)
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log("LEARN".length === 5 && "Student".length === 7)
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log("LEARN".length === 5 || "Student".length === 10)
 // Does "LEARN" contain the subset "RN"?
 // Does "LEARN" contain the subset "rn"?
 // Does "LEARN"[0] strictly equal "l"?
