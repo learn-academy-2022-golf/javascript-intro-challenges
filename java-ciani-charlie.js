e0c81451a5830d5bcec8f89416e2065f60b776b9
Charlie19255 <charlie19255@gmail.com>
2022-10-26 13:58:13 -0700

    first part done

---
+++ java-ciani-charlie.js
@@ -0,0 +1,28 @@
+//Write the code that will log the outcome of 34 added to 71.
+console.log( 34 + 71 )
+// output 105
+//Write the code that will log the outcome of 67 subtracted from 123.
+console.log( 67 - 123 )
+// output -56
+//Write the code that will log the outcome of 56 multiplied by 23.
+console.log( 56 * 23 )
+// output 1288
+//Write the code that will log the outcome of 45 divided by 5.
+console.log( 45 / 5 )
+// output 9
+//Write the code that will log the outcome of 5 to the power of 7.
+console.log( 5 ** 7 )
+// output 78125
+//Write the code that will log the whole number remainder of 33 divided by 6.
+console.log( 33 % 6 )
+// output 3
+//Write the code that will log the length of a string containing your name.
+console.log("ciani".length)
+// output 5
+//Write the code that will log whether your string includes the letter "e"?
+console.log( "ciani".includes("e") )
+//Write the code that will log the character at the first index of the string.
+console.log( "ciani"[0])
+//Write the code that will log the string in all uppercase.
+var greeting = "ciani"
+console.log(greeting.toUpperCase())
