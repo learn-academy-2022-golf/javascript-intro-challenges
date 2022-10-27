52692189a530335d87c99093885ad206391bbba4
renitagid <renita.y.ip@gmail.com>
2022-10-27 11:54:12 -0700

    number challenges

--- arrays-mike-renita.js
+++ arrays-mike-renita.js
@@ -28,23 +28,34 @@ console.log(groceryList)
 
 // Consider the variable:
 
-// var numbers = [2, 4, 6, 8, 10]
+var numbers = [2, 4, 6, 8, 10]
 // Write the code that will add the number 0 to the beginning of the array.
+numbers.unshift(0)
+console.log(numbers)
 
 // Write the code that will add the number 12 to the end of the array.
+numbers.push(12)
+console.log(numbers)
 
 // Write the code that will remove the first number from the array.
+numbers.shift()
+console.log(numbers)
 
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+var zero = [0]
+console.log(zero.concat(numbers))
 
 // Consider the variable:
+var numSet = [2, 13, 6, 8, 4, 2]
 
-// var numSet = [2, 13, 6, 8, 4, 2]
 // Write the code that finds the index of the first appearance of the number 2.
+console.log(numSet.indexOf(2))
 
 // Write the code that finds the index of the last appearance of the number 2.
+console.log(numSet.lastIndexOf(2))
 
 // Write the code that returns the number at the third index.
+
 
 // Consider the variable:
 
