17c32b095c45a52a0c5c6ec4dedb6518d7e5d690
Charlie19255 <charlie19255@gmail.com>
2022-10-27 16:36:06 -0700

    done

--- jsloops-cs-bc.js
+++ jsloops-cs-bc.js
@@ -23,11 +23,11 @@ for(let i=1; i < 21; i++) {
 
 const nums = [3, 57, -9, 20, 67]
 // Create the code that will log the largest number from the array.
-    // let numSort = nums.sort()
-    // console.log(numSort.slice(4))
+let numSort = nums.sort()
 
+//console.log(numSort.slice(4))
 // Create the code that will log the smallest number from the array.
-// console.log(numSort.slice(0, 1))
+//console.log(numSort.slice(0, 1))
 // Create the code that will log the remainder of each number when divided by 2. Expected output: 1, 1, -1, 0, 1
 for(let i=0; i < nums.length; i++) {
     console.log(nums[i] % 2)
@@ -36,9 +36,40 @@ for(let i=0; i < nums.length; i++) {
 
 const myString = "learn student"
 // Create the code that will log the number of times the letter "e" occurs in the string.
+let count = 0
+for(let i=0; i < myString.length; i++) {
+    if (myString[i]=="e")
+    {count=count + 1;
+    }
+}
+console.log(count)
 // Create the code that will log every other character in the string.
+for(let i=0; i < myString.length; i=i+2)
+{console.log(myString[i])
+}
 // 🏔 Stretch Goals
 // Create the code that iterates from 5 to 15. For each iteration log if the current number is odd or even.
 // Expected output: "5 is odd" "6 is even" "7 is odd" ...etc
+const numbersOdd=[ 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+for(let i=0; i < numbersOdd.length; i++){
+if(numbersOdd[i] % 2 === 0) {
+    console.log(`${numbersOdd[i]} is even`)
+} else {
+    console.log(`${numbersOdd[i]} is odd`)
+}
+}
 // Fizz Buzz: Create the code that will iterate from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz.
 // Expected output: 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
+ for(let i=1; i < 101; i++) {
+     if([i] % 5 === 0 && [i] % 3 === 0) {
+         console.log("fizzbuzz")
+    } 
+   else if([i] % 5 === 0){
+         console.log("buzz")
+     }
+    else if ([i] % 3 === 0){
+        console.log("fizz")
+    }
+    else {console.log (i)}
+    
+ }
