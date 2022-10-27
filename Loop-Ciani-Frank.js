26fca2b9a95fc71facadf053893cb298c90f4634
cibrown94 <cianibrown94@aol.com>
2022-10-27 16:36:16 -0700

    We are great

--- Loop-Ciani-Frank.js
+++ Loop-Ciani-Frank.js
@@ -1,9 +1,9 @@
 // Create a for loop that logs each number from 1 - 20.
 
-        // for(let i = 1; i < 21; i++)
-        // {
-        //     console.log (i)
-        // }
+        for(let i = 1; i < 21; i++)
+        {
+            console.log (i)
+        }
 
 // output 
 
@@ -31,11 +31,11 @@
 
 // Create a for loop that logs every other number from 1 - 20.
 
-            // for(let i = 1; i < 20; i++){
-            //     if(i % 2 !==0){
-            //     console.log(i)
-            //      }
-            //     }
+            for(let i = 1; i < 20; i++){
+                if(i % 2 !==0){
+                console.log(i)
+                 }
+                }
 
 // output
 // 1
@@ -52,9 +52,9 @@
 
 // Create a for loop that logs the result of each number from 1 - 20 tripled.
 
-    // for(let i = 1; i < 20; i++){
-    //     console.log(i * 3)
-    //     }
+    for(let i = 1; i < 20; i++){
+        console.log(i * 3)
+        }
     
 // output 
 
@@ -80,12 +80,12 @@
 
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
 
-// for(let i = 1; i < 21; i++){
-//     if(i % 2 !==0){
-//         console.log("ODD")
-//     }
-//     else {console.log(i)}
-// }
+for(let i = 1; i < 21; i++){
+    if(i % 2 !==0){
+        console.log("ODD")
+    }
+    else {console.log(i)}
+/}
 
 // output
 // ODD
@@ -117,28 +117,28 @@
 
 // Create the code that will log the largest number from the array.
 
-// var max =0 
-// for(let i=0; i< nums.length; i ++)
-// {
-//     if (nums[i]> max){
-//         max= nums[i] 
-//     }
-// }
-// console.log(max)
+var max =0 
+for(let i=0; i< nums.length; i ++)
+{
+    if (nums[i]> max){
+        max= nums[i] 
+    }
+}
+console.log(max)
 
 // output
 // 67
 
 // Create the code that will log the smallest number from the array.
 
-// var min =0 
-// for(let i=0; i< nums.length; i ++)
-// {
-//     if (nums[i]< min){
-//         min= nums[i] 
-//     }
-// }
-// console.log(min)
+var min =0 
+for(let i=0; i< nums.length; i ++)
+{
+    if (nums[i]< min){
+        min= nums[i] 
+    }
+}
+console.log(min)
 
 // output 
 
@@ -149,18 +149,18 @@
 
 // the remainder of each number when divided by 2. 
 
-// const nums = [3, 57, -9, 20, 67]
 
-// for (let i = 0 ; i < nums.length ; i++)
-// {
-//     console.log(nums[i] % 2)
-// }
-// output:
-// 1
-// 1
-// -1
-// 0
-// 1
+
+for (let i = 0 ; i < nums.length ; i++)
+{
+    console.log(nums[i] % 2)
+}
+output:
+1
+1
+-1
+0
+1
 
 //Expected output: 1, 1, -1, 0, 1
 
@@ -169,17 +169,17 @@
 
 // Create the code that will log the number of times the letter "e" occurs in the string.
 
-// let count = 0
+let count = 0
 
-// for (var i = 0 ; i < myString.length ; i = i + 1)
-// {
-//     if (myString[i] === "e")
-//     {
-//         count = count + 1
-//     }
-// }
+for (var i = 0 ; i < myString.length ; i = i + 1)
+{
+    if (myString[i] === "e")
+    {
+        count = count + 1
+    }
+}
 
-// console.log(count)
+console.log(count)
 // output:
 // 2
 
@@ -212,6 +212,42 @@ for (let i = 0 ; i < myString.length ; i = i + 1)
 // 🏔 Stretch Goals
 
 // Create the code that iterates from 5 to 15. For each iteration log if the current number is odd or even.
+
+for (let i = 5 ; i < 16; i++ )
+{
+    if(i % 2 == 0)
+    {
+        console.log(`${i} is even`)
+        
+    }
+    else {
+        console.log(`${i} is odd`)
+    }
+}
+
+// output 
+
+// 5 is odd
+// 6 is even
+// 7 is odd
+// 8 is even
+// 9 is odd
+// 10 is even
+// 11 is odd
+// 12 is even
+// 13 is odd
+// 14 is even
+// 15 is odd
+
+
 // Expected output: "5 is odd" "6 is even" "7 is odd" ...etc
+
 // Fizz Buzz: Create the code that will iterate from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz.
+
+// replacing it with modulo 3 and 5 and the even/odd. i index 1- <100
+
+
+
+
+
 // Expected output: 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
