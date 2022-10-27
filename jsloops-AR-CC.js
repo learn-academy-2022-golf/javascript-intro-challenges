e03b752338d457915603516e357e71928e51845e
Shadow090509 <jamoso090509@gmail.com>
2022-10-27 16:36:53 -0700

    pushing

--- jsloops-AR-CC.js
+++ jsloops-AR-CC.js
@@ -1,42 +1,46 @@
 // Create a for loop that logs each number from 1 - 20.
 for (let i = 1; i < 21; i++) {
-    console.log(i);
+  console.log(i);
 }
 // Create a for loop that logs every other number from 1 - 20.
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+var numbers = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
 for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-        console.log(`${numbers[i]}`)
-    }
+  if (numbers[i] % 2 === 0) {
+    console.log(`${numbers[i]}`);
+  }
 }
 // Create a for loop that logs the result of each number from 1 - 20 tripled.
 for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i] * 3);
+  console.log(numbers[i] * 3);
 }
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
 // Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
 for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-        console.log(`${numbers[i]}`)
-    } else if (numbers[i] % 2 !== 0) {
-        console.log(`odd`);
-    }
+  if (numbers[i] % 2 === 0) {
+    console.log(`${numbers[i]}`);
+  } else if (numbers[i] % 2 !== 0) {
+    console.log(`odd`);
+  }
 }
 
 // Consider this variable:
 
-// const nums = [3, 57, -9, 20, 67]
+const nums = [3, 57, -9, 20, 67];
 // Create the code that will log the largest number from the array.
+let nums2 = nums.sort();
+console.log(nums2.slice(4));
 // Create the code that will log the smallest number from the array.
+// let nums2 = nums.sort();
+// console.log(nums2.slice(0, 1));
 // Create the code that will log the remainder of each number when divided by 2. Expected output: 1, 1, -1, 0, 1
-
 
 // Consider this variable:
 
 // const myString = "learn student"
 // Create the code that will log the number of times the letter "e" occurs in the string.
 // Create the code that will log every other character in the string.
-
 
 // 🏔 Stretch Goals
 // Create the code that iterates from 5 to 15. For each iteration log if the current number is odd or even.
