fb8cc71350ef21fb82789243c7607a3b62ebc135
BCarta7 <cannolidude@msn.com>
2022-10-28 11:36:24 -0700

    passing to bella

--- jsfunctions-kenny-blake-annabella.js
+++ jsfunctions-kenny-blake-annabella.js
@@ -30,15 +30,64 @@ const oddOrEven = (num) => {
 }
 console.log(oddOrEven(4))
 // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
-
+//output: num *3
+//input: num
+const triple = (num) => {
+    return num * 3
+}
+console.log(triple(2))
 // Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
-
+//output: * result
+//input: num1 * num2
+const multiply = (num1, num2) => {
+    return num1 * num2
+}
+console.log(multiply(5, 3))
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
-
-// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+//output: "num1 is evenly divisible by num2"
+//input: num1/num2
+// 
+const divisibleBy = (num1, num2) => {
+    if (num1 % num2 === 0) {
+        return `${num1} is evenly divisible by ${num2}`
+    } else {
+        return `${num1} is NOT evenly divisible by ${num2}`
+    }
+}
+console.log(divisibleBy(10, 2))
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score. 
+//output: letter grade
+//input: number score
+//method:
+const assignGrade = (score) => {
+    if (score > 90) {
+        return "A"
+    } else if (score >= 80) {
+        return "B"
+    } else if (score >= 70) {
+        return "C"
+    } else if (score >= 60) {
+        return "D"
+    } else {
+        return "F"
+    }
+}
+console.log(assignGrade(2))
 
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
-
+//
+//
+//
+const isLonger = (string1, string2) => {
+    if(string1.length > string2.length) {
+        return `${string1}`
+    } else if(string1.length < string2.length) {
+        return`${string2}`
+    } else {
+        return "why are you doing this?"
+    }
+}
+console.log(isLonger("ABCDEF", "ABCDEF"))
 // Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
 
 // -. Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
