2fe7377b7a8dcf605095a4af02e310a0f1daf19f
Charlie19255 <charlie19255@gmail.com>
2022-10-28 11:20:28 -0700

    first part done

---
+++ jsfunc-charlie-zeke.js
@@ -0,0 +1,88 @@
+// Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt. Each solution requires pseudocode. Test your functions with multiple function calls.
+
+// Write a function named marco that returns "polo".
+//input marco()
+//output "polo"
+// const marco = () =>{
+//     return "polo"
+
+// }
+// console.log(marco())
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+//input greeting("name")
+//output "welcome, "name"!"
+// const greeting = (name) =>{
+//     return `Welcome ${name}!`
+// }
+// console.log(greeting("Zeke"))
+
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+//input oddOrEven("number")
+//output "odd or even"
+
+// const oddOrEven  = (num) =>{
+//      if(num % 2 !== 0)
+//      return "odd"
+//      else 
+//      return "even"
+// }
+
+// console.log(oddOrEven(46))
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+//input triple("number")
+//output ("number *3")
+// const triple  = (num) =>{
+//     return(num * 3)
+    
+// }
+
+// console.log(triple(1))
+// Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+//input multiply(num1,num2)
+//output (num1*num2)
+// const multiply  = (num1, num2) =>{
+//         return(num1 * num2)
+        
+//     }
+//     console.log(multiply(3, 4))
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+//input divisbleBy(num1,num2)
+//output (if num1 divible by num2)
+// const divisibleBy  = (num1, num2) =>{
+//         if(num1 % num2 === 0)
+//     return `${num1} is divisible by ${num2}.`
+//         else return `${num1} is not divisible by ${num2}.`
+        
+//     }
+//    console.log(divisibleBy(9, 5))
+
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+//input assignGrade(number)
+//output assignGrade(letter)
+// const assignGrade=(num)=>{
+//     if(num>90)
+//     return "A"
+//     else if(num>80)
+//     return "B"
+//     else if(num>70)
+//     return "C"
+//     else 
+//     return "Failed"
+// }
+// console.log(assignGrade(60))
+
+// Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+
+// Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+
+// -. Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
+
+// 🏔 Stretch Goals
+// The World Translator
+
+// Write a function named helloWorld that takes a language code (e.g. "es", "de", "en") as an argument and returns "Hello World!" in the given language. Ensure you function works for at least 5 languages.
+// Have your function default to returning English.
+// The Pluralizer
+
+// Write a function named pluralizer that takes a number and a singular noun as arguments and returns the number and pluralized form of the noun, if necessary.
+// Enhance your function so it can handle a few collective nouns like "sheep", "goose", "child", "person" and "species".
